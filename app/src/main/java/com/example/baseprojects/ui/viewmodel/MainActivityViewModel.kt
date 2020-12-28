836e45f8ec3760a1e89af69e8f969aa91c386a7d
QuangTD <tdquang252@gmail.com>
2020-12-28 09:41:57 +0700

    link tinh

--- app/src/main/java/com/example/baseprojects/ui/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/baseprojects/ui/viewmodel/MainActivityViewModel.kt
@@ -1,10 +1,8 @@
 package com.example.baseprojects.ui.viewmodel
 
-import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import com.example.baseprojects.common.BaseViewModel
-import com.example.baseprojects.common.Constants
 import com.example.domain.model.MainResponse
 import com.example.domain.repository.BaseUseCase
 import com.example.domain.usecase.GetTryApiUseCase
@@ -12,12 +10,9 @@ import com.example.domain.usecase.GetTryApiUseCase
 class MainActivityViewModel @ViewModelInject constructor(getTryApiUseCase: GetTryApiUseCase) :
     BaseViewModel() {
     private val _getDataApi: LiveData<MainResponse?> = liveDataEmit {
-        Log.d(Constants.TAG, "_getDataApi")
-            getTryApiUseCase.invoke(BaseUseCase.Param())
+        getTryApiUseCase.invoke(BaseUseCase.Param())
     }
 
 
-    val getDataApi: LiveData<MainResponse?> = _getDataApi.switchMapLiveDataEmit {
-        it
-    }
+    val getDataApi: LiveData<MainResponse?> = _getDataApi
 }
---
+++ data/src/main/java/com/example/data/local/database/BaseLocalDao.kt
@@ -0,0 +1,9 @@
+package com.example.data.local.database
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface BaseLocalDao<Result> {
+
+}

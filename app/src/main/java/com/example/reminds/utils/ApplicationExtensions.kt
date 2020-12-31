78506cb7308b0a5a27ae8022456056588fad06de
QuangTD <tdquang252@gmail.com>
2020-12-31 10:13:27 +0700

    keyboard utils

--- app/src/main/java/com/example/reminds/utils/ApplicationExtensions.kt
+++ app/src/main/java/com/example/reminds/utils/ApplicationExtensions.kt
@@ -2,8 +2,35 @@ package com.example.reminds.utils
 
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
 fun Fragment.navigation(@IdRes resId: Int) {
     NavHostFragment.findNavController(this).navigate(resId)
+}
+
+fun Fragment.hideSoftKeyboard() {
+    view?.findFocus()?.let { KeyboardUtils.hideSoftKeyboard(it, requireContext()) }
+}
+
+fun Fragment.navigate(
+    direction: NavDirections,
+    navOptions: NavOptions? = null,
+    navExtras: Navigator.Extras? = null
+) {
+    try {
+        findNavController().navigate(direction.actionId, direction.arguments, navOptions, navExtras)
+    } catch (E: Exception) {
+    }
+}
+
+fun Fragment.finish() {
+    if (!findNavController().navigateUp()) requireActivity().finish()
+}
+
+fun Fragment.navigateUp() {
+    findNavController().navigateUp()
 }
---
+++ app/src/main/java/com/example/reminds/utils/KeyboardUtils.kt
@@ -0,0 +1,27 @@
+package com.example.reminds.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+class KeyboardUtils {
+    companion object {
+        fun showKeyboard(editText: EditText, context: Context?) {
+            if (context == null) return
+
+            editText.requestFocus()
+            val inputMethodManager = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+        }
+
+        fun hideSoftKeyboard(focus: View, context: Context?) {
+            if (context == null) return
+
+            focus.clearFocus()
+            val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(focus.windowToken, 0)
+        }
+    }
+}

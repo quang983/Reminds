112855d8a0cce5ad97b63c24e3d035112739e757
QuangTD <tdquang252@gmail.com>
2021-02-20 19:27:16 +0700

    ads google

--- app/src/main/java/com/example/reminds/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/reminds/ui/activity/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.example.reminds.ui.activity
 
-/*import com.google.android.gms.ads.*
-import com.google.android.gms.ads.rewarded.RewardItem
-import com.google.android.gms.ads.rewarded.RewardedAd*/
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.ComponentName
@@ -10,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
@@ -20,11 +18,15 @@ import androidx.navigation.ui.NavigationUI
 import com.example.common.base.model.AlarmNotificationEntity
 import com.example.framework.local.cache.CacheImpl.Companion.KEY_FIRST_LOGIN
 import com.example.framework.local.cache.CacheImpl.Companion.SHARED_NAME
+import com.example.reminds.BuildConfig
 import com.example.reminds.R
 import com.example.reminds.service.INSERT_OBJECT_TIMER_DATA
 import com.example.reminds.service.NotificationService
 import com.example.reminds.ui.sharedviewmodel.MainActivityViewModel
 import com.example.reminds.utils.postValue
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.play.core.review.ReviewManagerFactory
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.content_main.*
@@ -40,6 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     //    private var mRewardedAd: RewardedAd? = null
     private var TAG = "logMain"
+    private var mInterstitialAd: InterstitialAd? = null
 
     /** Messenger for communicating with the service.  */
     private var mMessenger: Messenger? = null
@@ -57,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         checkAddFirstTopic()
         catchEventKeyboard()
         setObserver()
-//        createAdsMode()
+        createAdsMode()
     }
 
     private fun setObserver() {
@@ -65,9 +68,9 @@ class MainActivity : AppCompatActivity() {
             notifyDataInsert.observe(this@MainActivity, {
                 sendActionInsertAlert(it)
             })
-            /*showAdsMobile.observe(this@MainActivity, {
+            showAdsMobile.observe(this@MainActivity, {
                 showAdsMobile()
-            })*/
+            })
         }
     }
 
@@ -162,6 +165,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createAdsMode() {
+        MobileAds.initialize(this)
+    /*    MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("ABCDEF012345"))
+                .build()
+        )*/
+        val adRequest = AdRequest.Builder().build()
+
+        if(BuildConfig.DEBUG){
+            InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d(TAG, adError.message)
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d(TAG, "Ad was loaded.debug")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+        }else{
+            InterstitialAd.load(this,"ca-app-pub-9829869928534139/3052159234", adRequest, object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d(TAG, adError.message)
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d(TAG, "Ad was loaded. release")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+        }
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
         /* MobileAds.initialize(this) {
          }
          val adRequest = AdRequest.Builder().build()
@@ -198,6 +248,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showAdsMobile() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
+
         /*    mRewardedAd?.show(this) {
                 fun onUserEarnedReward(rewardItem: RewardItem) {
                     var rewardAmount = rewardItem.amount

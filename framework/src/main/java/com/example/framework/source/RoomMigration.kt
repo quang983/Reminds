6a56bb6e4cad6fc7ecdbebd9977913fb11887570
QuangTD <tdquang252@gmail.com>
2021-04-13 11:38:02 +0700

    migration

--- framework/src/main/java/com/example/framework/source/RoomMigration.kt
+++ framework/src/main/java/com/example/framework/source/RoomMigration.kt
@@ -9,7 +9,7 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN isShowContents INTEGER DEFAULT 0 NOT NULL")
         database.execSQL("ALTER TABLE TopicGroup ADD COLUMN optionSelected INTEGER DEFAULT 0 NOT NULL")
         database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN hashTag INTEGER DEFAULT 0 NOT NULL")
-        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerReminder LONG DEFAULT 0 NOT NULL")
+        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerReminder INTEGER DEFAULT 0 NOT NULL")
     }
 }
 
@@ -17,7 +17,7 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
 val MIGRATION_2_3 = object : Migration(2, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE TopicGroup ADD COLUMN typeTopic INTEGER DEFAULT 0 NOT NULL")
-        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN createTime LONG DEFAULT 0 NOT NULL")
+        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN createTime INTEGER DEFAULT 0 NOT NULL")
         database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN stt INTEGER DEFAULT 0 NOT NULL")
         database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN description STRING NOT NULL")
         database.execSQL("ALTER TABLE TopicGroup ADD COLUMN imageSource INTEGER")
@@ -26,6 +26,6 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
 
 val MIGRATION_3_4 = object : Migration(3, 4) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerFocus LONG")
+        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerFocus INTEGER")
     }
 }

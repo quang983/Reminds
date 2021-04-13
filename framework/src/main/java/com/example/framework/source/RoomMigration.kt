08ffd2acf4ffd81e00f81ca7257a1a063a04819a
QuangTD <tdquang252@gmail.com>
2021-04-13 10:39:52 +0700

    update version

--- framework/src/main/java/com/example/framework/source/RoomMigration.kt
+++ framework/src/main/java/com/example/framework/source/RoomMigration.kt
@@ -26,6 +26,6 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
 
 val MIGRATION_3_4 = object : Migration(3, 4) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerFocus LONG DEFAULT 0 NOT NULL")
+        database.execSQL("ALTER TABLE WorkFoTopic ADD COLUMN timerFocus LONG")
     }
 }

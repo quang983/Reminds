6661e7f86b70c389c2cce3f8b592783224dadef5
QuangTD <tdquang252@gmail.com>
2021-05-14 21:01:37 +0700

    migration success

--- framework/src/main/java/com/example/framework/source/RoomMigration.kt
+++ framework/src/main/java/com/example/framework/source/RoomMigration.kt
@@ -42,8 +42,8 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
 //chua golive
 val MIGRATION_4_5 = object : Migration(4, 5) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("CREATE TABLE IF NOT EXISTS `DailyTask` (`id` INTEGER DEFAULT 0 NOT NULL, `name` TEXT DEFAULT '' NOT NULL, `createTime` INTEGER DEFAULT 0 NOT NULL,`endTime` INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (`id`))")
+        database.execSQL("CREATE TABLE IF NOT EXISTS `DailyTask` (`id` INTEGER DEFAULT 0 NOT NULL, `name` TEXT DEFAULT ' ' NOT NULL, `createTime` INTEGER DEFAULT 0 NOT NULL,`endTime` INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (`id`))")
 
-        database.execSQL("CREATE TABLE IF NOT EXISTS `DailyDivideTaskDone` (`id` INTEGER DEFAULT 0 NOT NULL,`idGroup` INTEGER DEFAULT 0 NOT NULL, `name` TEXT DEFAULT '' NOT NULL, `doneTime` INTEGER DEFAULT 0 NOT NULL,`remainingTime` INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (`id`),FOREIGN KEY (`idGroup`) REFERENCES `DailyTask`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE)")
+        database.execSQL("CREATE TABLE IF NOT EXISTS `DailyDivideTaskDone` (`id` INTEGER DEFAULT 0 NOT NULL,`idGroup` INTEGER DEFAULT 0 NOT NULL, `name` TEXT DEFAULT ' ' NOT NULL, `doneTime` INTEGER DEFAULT 0 NOT NULL,`remainingTime` INTEGER DEFAULT 0 NOT NULL, PRIMARY KEY (`id`))")
     }
 }
